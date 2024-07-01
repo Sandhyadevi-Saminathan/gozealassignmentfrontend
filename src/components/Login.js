2036fa9a47a2b8df15eacc965b27212e78155a62
Sandhya devi Saminathan <sandhya021999@gmail.com>
2024-07-01 12:28:38 +0530

    commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,12 +5,10 @@ import { useFormik } from 'formik';
 import { loginUser } from '../actions/authActions'; 
 import { useNavigate } from 'react-router-dom';
 
-
 const Login = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate(); // Access navigate function
+  const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = React.useState(null);
-
 
   const formik = useFormik({
     initialValues: {
@@ -18,90 +16,100 @@ const Login = () => {
       password: '',
     },
     validate: values => {
-        const errors = {};
-        if (!values.email) {
-            errors.email = 'Please enter Email';
-        }
-        if (!values.password) {
-            errors.password = 'Please enter Password';
-        }
-        return errors;
+      const errors = {};
+      if (!values.email) {
+        errors.email = 'Please enter Email';
+      }
+      if (!values.password) {
+        errors.password = 'Please enter Password';
+      }
+      return errors;
     },
-    onSubmit: async (values) => {
-        setErrorMessage(null); // Clear previous errors
-        try {
-          const userData = {
-            email: values.email,
-            password: values.password,
-          };
-          
-          const response = await dispatch(loginUser(userData));
-          if (response && response.token) {
-            console.log("Logged In");
-            alert('Logged in successfully!');
-            navigate('/projectlist');
-          } else {
-            throw new Error('Login failed, please check your credentials.');
-          }
-        } catch (error) {
-          setErrorMessage(error.response?.data?.message || 'Login failed. Please check your credentials.');
-          console.log('Error during login:', error);
+    onSubmit: async (values, { resetForm }) => {
+      setErrorMessage(null); // Clear previous errors
+      try {
+        const userData = {
+          email: values.email,
+          password: values.password,
+        };
+        
+        const response = await dispatch(loginUser(userData));
+        if (response && response.token) {
+          console.log("Logged In");
+          alert('Logged in successfully!');
+          navigate('/projectlist');
+        } else {
+          throw new Error('Login failed, please check your credentials.');
         }
-      },
+      } catch (error) {
+        setErrorMessage(error.response?.data?.message || 'Login failed. Please check your credentials.');
+        console.log('Error during login:', error);
+
+        // Hide error message after 10 seconds
+        setTimeout(() => {
+          setErrorMessage(null);
+          resetForm();
+        }, 10000);
+      }
+    },
   });
 
   return (
     <div className="container mt-5">
-    <div className="row justify-content-center">
+      <div className="row justify-content-center">
         <div className="col-md-9 col-lg-6 col-xl-4 shadow p-3 mb-5 rounded">
-            <div className="p-5" style={{ color: 'tomato', fontSize: '18px', fontFamily: 'cursive' }}>
-                <div className="text-center">
-                    <h1 className="h4 text-gray-900 mb-4">Welcome!</h1>
-                </div>
-                 {/* Login form */}
-                <form onSubmit={formik.handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            id="email"
-                            name="email"
-                            type="email"
-                            className={`form-control ${formik.errors.email ? 'is-invalid' : ''}`}
-                            onChange={formik.handleChange}
-                            value={formik.values.email}
-                            placeholder="Enter your Email"
-                            style={{ border: '2px solid skyblue' }}
-                        />
-                        <span className="text-danger">{formik.errors.email}</span>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            className={`form-control ${formik.errors.email ? 'is-invalid' : ''}`}
-                            onChange={formik.handleChange}
-                            value={formik.values.password}
-                            placeholder="Enter your password"
-                            style={{ border: '2px solid skyblue' }}
-                        />
-                        <span className="text-danger">{formik.errors.password}</span>
-                    </div>
-                    {errorMessage && <div className="alert alert-danger mt-3">{errorMessage}</div>}
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-info rounded col-lg-12 mt-4">Log in</button>
-                    </div>
-                    <hr />
-                </form>
-      
-                <div className="text-center mt-2">
-                    <Link to="/register">Create an Account!</Link>
-                </div>
+          <div className="p-5" style={{ color: 'tomato', fontSize: '18px', fontFamily: 'cursive' }}>
+            <div className="text-center">
+              <h1 className="h4 text-gray-900 mb-4">Welcome!</h1>
             </div>
+            {/* Error message */}
+            {errorMessage && (
+              <div className="alert alert-danger mt-3" style={{ position: 'fixed', top: '20px', right: '20px', zIndex: 1000 }}>
+                {errorMessage}
+              </div>
+            )}
+            {/* Login form */}
+            <form onSubmit={formik.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  className={`form-control ${formik.errors.email ? 'is-invalid' : ''}`}
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                  placeholder="Enter your Email"
+                  style={{ border: '2px solid skyblue' }}
+                />
+                <span className="text-danger">{formik.errors.email}</span>
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  className={`form-control ${formik.errors.password ? 'is-invalid' : ''}`}
+                  onChange={formik.handleChange}
+                  value={formik.values.password}
+                  placeholder="Enter your password"
+                  style={{ border: '2px solid skyblue' }}
+                />
+                <span className="text-danger">{formik.errors.password}</span>
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-info rounded col-lg-12 mt-4">Log in</button>
+              </div>
+              <hr />
+            </form>
+            <div className="text-center mt-2">
+              <Link to="/register">Create an Account!</Link>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
   );
 };
 
